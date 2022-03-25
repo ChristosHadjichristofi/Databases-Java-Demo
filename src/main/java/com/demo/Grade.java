50691d36355ef4ae2e357dcf6d7b6c5eb4f1bdc6
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2022-03-25 18:48:08 +0200

    Add constructor with id, add getId method

--- src/main/java/com/demo/Grade.java
+++ src/main/java/com/demo/Grade.java
@@ -2,9 +2,17 @@ package com.demo;
 
 public class Grade {
 
+    private Integer id;
     private String course_name;
     private int grade;
     private int student_id;
+
+    public Grade(Integer id, String course_name, int grade, int student_id) {
+        this.id = id;
+        this.course_name = course_name;
+        this.grade = grade;
+        this.student_id = student_id;
+    }
 
     public Grade(String course_name, int grade, int student_id) {
         this.course_name = course_name;
@@ -12,6 +20,7 @@ public class Grade {
         this.student_id = student_id;
     }
 
+    public Integer getId() { return this.id; }
     public int getGrade() { return this.grade; }
     public int getStudentID() { return this.student_id; }
     public String getCourseName() { return this.course_name; }
